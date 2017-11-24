3483a0a9652682158c06e051aea26e473782eeb9
Sylwia Grzegolec <s.grzegolec@gmail.com>
2017-11-24 22:58:26 +0100

    Code refactoring and code styling improvements for video view

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -5,32 +5,36 @@ import { RestService } from '../../services/rest.service';
 import { Video } from '../../data-models/video-data-model';
 
 @Component({
-  selector: 'yv-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'yv-video',
+	templateUrl: './video.component.html',
+	styleUrls: ['./video.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class VideoComponent implements OnInit {
-  public videoUrl: SafeUrl;
-  public video: Video = new Video();
-  private VIDEO_URL_BASE = 'https://www.youtube-nocookie.com/embed/';
-  private VIDEO_URL_PARAMS = '?rel=0&vq=hd1080';
+	public videoUrl: SafeUrl;
+	public video: Video = new Video();
+	private VIDEO_URL_BASE = 'https://www.youtube-nocookie.com/embed/';
+	private VIDEO_URL_PARAMS = '?rel=0&vq=hd1080';
 
-  constructor(private route: ActivatedRoute,
-              private sanitizer: DomSanitizer,
-              private restService: RestService) {
-  }
+	constructor(private route: ActivatedRoute,
+	            private sanitizer: DomSanitizer,
+	            private restService: RestService) {
+	}
 
-  ngOnInit() {
-    this.route.params.subscribe((params) => {
-      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.VIDEO_URL_BASE.concat(params.videoId, this.VIDEO_URL_PARAMS));
-      this.restService.getVideoData(params.videoId).subscribe((data) => {
-        const fetchedVideoData = data['items'][0];
-        console.log(fetchedVideoData);
-        this.video.title = fetchedVideoData.snippet.title;
-        this.video.description = fetchedVideoData.snippet.description;
-        this.video.tags = fetchedVideoData.snippet.tags;
-      });
-    });
-  }
+	ngOnInit() {
+		this.route.params.subscribe((params) => {
+			this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.VIDEO_URL_BASE.concat(params.videoId, this.VIDEO_URL_PARAMS));
+
+			this.restService.getVideoData(params.videoId).subscribe((data) => {
+				const videoSnippetData = data['items'][0];
+				this.processVideoData(videoSnippetData);
+			});
+		});
+	}
+
+	private processVideoData = (videoSnippetData) => {
+		this.video.title = videoSnippetData.title;
+		this.video.description = videoSnippetData.description;
+		this.video.tags = videoSnippetData.tags;
+    }
 }
